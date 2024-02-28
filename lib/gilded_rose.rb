ae77c17ebe1f932c5a50c4f5af8b620e32204396
Stilyan Dimitrov <sti4o_21@aol.com>
2024-02-27 23:34:14 -0600

    fixed normal

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,6 +8,12 @@ class GildedRose
   end
 
   def tick
+
+    if name == 'Normal Item'
+      return normal_tick
+    end 
+
+    
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
         if @name != "Sulfuras, Hand of Ragnaros"
@@ -52,4 +58,18 @@ class GildedRose
       end
     end
   end
+
+  def normal_tick
+    if @quality != 0
+      if @days_remaining > 0 
+        @quality -= 1
+      end
+      if @days_remaining <= 0 
+        @quality -=2
+      end 
+    end   
+    @days_remaining -= 1
+  end
+
+
 end
